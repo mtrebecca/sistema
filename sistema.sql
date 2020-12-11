5f97fa9c50aaa31594cbd0321721008203f07937
mtrebecca <mtrebeccamartins@gmail.com>
2020-12-11 17:47:28 -0300

    First commit

---
+++ sistema.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin
+
+-- Database: `sistema`
+--
+--tabela `categorias`
+--
+ `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nome_categoria` varchar(100) NOT NULL,
+  `dataCaptura` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabela `clientes`
+
+`clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `sobrenome` varchar(100) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(100) NOT NULL,
+  `cpf` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabela `fornecedores`
+ `fornecedores` (
+  `id_fornecedor` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `sobrenome` varchar(100) NOT NULL,
+  `endereco` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(100) NOT NULL,
+  `cpf` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabela `imagens`
+
+`imagens` (
+  `id_imagem` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `dataUpload` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabela `produtos`
+
+`produtos` (
+  `id_produto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_imagem` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `preco` float NOT NULL,
+  `dataCaptura` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabela `usuarios`
+ `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `dataCaptura` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- tabela `vendas`
+`vendas` (
+  `id_venda` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `preco` float NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `total_venda` float NOT NULL,
+  `dataCompra` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
---
+++ sistema/lib/dompdf/autoload.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/lib/html5lib/Parser.php';
+require_once __DIR__ . '/lib/php-font-lib/src/FontLib/Autoloader.php';
+require_once __DIR__ . '/lib/php-svg-lib/src/autoload.php';
+
+
+require_once __DIR__ . '/src/Autoloader.php';
+
+Dompdf\Autoloader::register();
